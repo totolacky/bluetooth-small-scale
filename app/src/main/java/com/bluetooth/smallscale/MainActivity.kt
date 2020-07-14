70c26e9aa01720b654f61cc54b26a31949af0a1e
totolacky <totolacky@kaist.ac.kr>
2020-07-14 22:36:36 +0900

    Create template

--- app/src/main/java/com/bluetooth/smallscale/MainActivity.kt
+++ app/src/main/java/com/bluetooth/smallscale/MainActivity.kt
@@ -1,23 +1,70 @@
 package com.bluetooth.smallscale
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Environment
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import java.io.File
+import java.io.FileOutputStream
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var deviceId: String = ""
+    private val FILE_NAME: String = "EncounterLog.txt"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        // Check for permissions
+        var reqPermissions: Array<String> = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        ActivityCompat.requestPermissions(this, reqPermissions, 0)
+
+        findViewById<Button>(R.id.update_id_button).setOnClickListener {
+            deviceId = findViewById<TextInputEditText>(R.id.newId).text.toString()
+            findViewById<TextView>(R.id.deviceIdText).text = "ID: "+deviceId
+            writeFile(it, FILE_NAME, "hahahahahahahaha")
+            pushMsg(deviceId)
+            Snackbar.make(it, deviceId, Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+    }
+
+    fun isExternalStorageWritable(): Boolean {
+        return if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            Log.i("State","Yes, it is writable!")
+            true
+        } else {
+            false
+        }
+    }
+
+    // Write the given message to /Download/fname
+    fun writeFile(view: View, fname: String, msg: String) {
+        if(isExternalStorageWritable()) {
+            val newMsg: String = "\n"+msg
+            var myFile: File = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fname)
+            var fstream: FileOutputStream = FileOutputStream(myFile, true)
+            fstream.write(newMsg.toByteArray())
+            fstream.close()
+        }
+    }
+
+    // Write the given message to the top of the message log
+    fun pushMsg(msg: String) {
+        findViewById<TextView>(R.id.recentMeg4).text = findViewById<TextView>(R.id.recentMeg3).text
+        findViewById<TextView>(R.id.recentMeg3).text = findViewById<TextView>(R.id.recentMeg2).text
+        findViewById<TextView>(R.id.recentMeg2).text = findViewById<TextView>(R.id.recentMeg1).text
+        findViewById<TextView>(R.id.recentMeg1).text = msg
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
